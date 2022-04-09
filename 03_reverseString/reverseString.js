bd927dbe970459b522fa24e1318e2e4a64bb4256
Mengxi He <mengxihex@gmail.com>
2022-04-09 14:29:52 +0200

    finished the reversString and removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split('');
+    let reversedString = splitString.reverse();
+    let joinString = reversedString.join('');
+    return joinString;
 };
 
 // Do not edit below this line
