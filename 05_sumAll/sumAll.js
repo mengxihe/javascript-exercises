d0c105a63ab4717f5f29a4cb2a6d8a911d578f90
Mengxi He <mengxihex@gmail.com>
2022-04-09 15:41:07 +0200

    finished the 05_sumAll 06_leapYears and 07_tempConversion exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1<0 ||num2<0){
+        return 'ERROR';
+    }
+    if (typeof num1 !== 'number' ||typeof num2 !== 'number'){
+        return 'ERROR'
+    }
+    let sum = 0;
+    let temp = 0;
+    for (let i = 0; i<=(Math.abs((num2-num1))); i++){
+        temp += 1;
+        sum = sum+temp;
+    }
+    return sum;
+    
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    celsius = ((temp-32))*(5/9);
+    let rounded = Math.round(celsius * 10)/10;
+    return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    fahrenheit = (temp*(9/5))+32;
+    let rounded = Math.round(fahrenheit * 10) / 10;
+    return rounded;
 };
 
 // Do not edit below this line
