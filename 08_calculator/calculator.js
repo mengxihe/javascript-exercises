d63e18d791b6770732634e7a551d5068b81f53fb
Mengxi He <mengxihex@gmail.com>
2022-04-24 19:00:33 +0200

    finished exercise;

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return (numbers.reduce((a, b) => a+b, 0));
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return (array.reduce((a, b) => a*b));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (Math.pow(a, b));
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	return (x>1) ? x * factorial(x-1) : 1;
 };
 
 // Do not edit below this line
