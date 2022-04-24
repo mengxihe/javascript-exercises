7c660eb9b783756a5e56392c12982d839f2fa4d8
Mengxi He <mengxihex@gmail.com>
2022-04-24 19:17:36 +0200

    finished palindromes exercise;

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let re = /[\W_]/g
+    let lowString = string.toLowerCase().replace(re, '');
+    let reverseString = lowString.split('').reverse().join('');
+    return reverseString === lowString;
 };
 
 // Do not edit below this line
